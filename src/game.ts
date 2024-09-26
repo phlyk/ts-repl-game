bfe533c82ffbf2ef2bb281e6fee4efcaee87ad63
phlyk <philippe.keighley@erm.com>
2024-09-26 12:16:05 +0100

    Initial commit of game repo. Working REPL

---
+++ src/game.ts
@@ -0,0 +1,15 @@
+export class Game {
+
+    public showActions() {
+        console.log("\nChoose an action:");
+        console.log("1: This is where");
+        console.log("2: You will");
+        console.log("3: Define what your game");
+        console.log("4: Can do");
+        console.log("5: Don't forget to quit");
+    }
+
+    public handleAction(input: string): boolean {
+        return false;
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import readline from "readline";
+import { Game } from "./game";
+
+// Initialize the game
+const game = new Game();
+
+// Setup the readline interface
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// Start the game loop
+function startGame(isFinished = false) {
+    if (isFinished) {
+        console.log("Game Over");
+        rl.close();
+    }
+
+    game.showActions();
+
+    rl.question("Enter your choice: ", (input: string) => {
+        isFinished = game.handleAction(input);
+        startGame();  // Loop back to show actions again
+    });
+}
+
+console.log("Welcome to the Battle Game!");
+startGame();
